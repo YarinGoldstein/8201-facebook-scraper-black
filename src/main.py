63f4667ae5bb0361937031d017462a009c5cbef5
daniel rogel <sagyrogel1@gmail.com>
2021-06-07 04:19:41 -0700

    feat: adds id field for target profile.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import facebook_scraper as scraper
 
-cur_credentials = ("danielking1002@walla.com","1101999d")
-target_profile_name = 'shirly.dwek'
+target_profile = {'id': "123456789", 'name': "shirly.dwek"}
+cur_credentials = ("danielking1002@walla.com", "1101999d")
 
-target_posts = [_ for _ in scraper.get_posts(target_profile_name,credentials=cur_credentials, pages=1)]
-print(target_posts)
-# print(scraper.get_profile('daniel.rogel.58'))
+target_posts = [_ for _ in scraper.get_posts(
+    target_profile['name'], credentials=cur_credentials, pages=1)]
+formatted_posts = [curPost for curPost in target_posts]
