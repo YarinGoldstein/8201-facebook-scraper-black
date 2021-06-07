4441735a8d4ac1a5b1c51822fa7d16e288a0578b
daniel rogel <sagyrogel1@gmail.com>
2021-06-07 06:41:45 -0700

    feat: links date-string formatting function to main script.

---
+++ src/dateString.py
@@ -0,0 +1,2 @@
+def format(input_date):
+    return 'd'
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 import facebook_scraper as scraper
+import dateString
 
 target_profile = {'id': "123456789", 'name': "shirly.dwek"}
 cur_credentials = ("danielking1002@walla.com", "1101999d")
 
 target_posts = [_ for _ in scraper.get_posts(
     target_profile['name'], credentials=cur_credentials, pages=1)]
-formatted_posts = [curPost for curPost in target_posts]
+formatted_posts = [{'text': curPost['post_text'],
+                    'publish_date': curPost['time'],
+                    'scrape_date': dateString.format(datetime.now())} for curPost in target_posts]
+print("done")
