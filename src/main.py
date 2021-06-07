2b989cfbd7bfcad240ee52650547bd549e31bc1d
daniel rogel <sagyrogel1@gmail.com>
2021-06-07 03:53:47 -0700

    feat: fetches post data.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import facebook_scraper as scraper
+
+cur_credentials = ("danielking1002@walla.com","1101999d")
+target_profile_name = 'shirly.dwek'
+
+target_posts = [_ for _ in scraper.get_posts(target_profile_name,credentials=cur_credentials, pages=1)]
+print(target_posts)
+# print(scraper.get_profile('daniel.rogel.58'))
