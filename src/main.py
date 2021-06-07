b4d5384609dd00450f911bcbef47cd732f631129
daniel rogel <sagyrogel1@gmail.com>
2021-06-07 07:42:07 -0700

    feat: adds external traget profiles.

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ target_profile = {'id': "123456789", 'name': "shirly.dwek"}
 cur_credentials = ("danielking1002@walla.com", "1101999d")
 date_format_string = "%Y-%m-%dT%H:%M:%S"
 
-target_posts = [_ for _ in scraper.get_posts(
-    target_profile['name'], credentials=cur_credentials, pages=1)]
+target_posts = scraper.get_posts(
+    target_profile['name'], credentials=cur_credentials, pages=1)
 formatted_posts = [{'text': curPost['post_text'],
                     'publish_date': (curPost['time'].strftime(date_format_string)
                                      if curPost['time'] is not None
                                      else None),
-                    'scrape_date': datetime.now().strftime(date_format_string)}
+                    'scraping_date': datetime.now().strftime(date_format_string)}
                    for curPost in target_posts]
 print("done")
