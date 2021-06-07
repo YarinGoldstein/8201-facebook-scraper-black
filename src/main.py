782d596a5cbcec9186b6786607dd678658576a94
daniel rogel <sagyrogel1@gmail.com>
2021-06-07 09:37:58 -0700

    feat: renames variables to match post groups.

--- src/main.py
+++ src/main.py
@@ -5,15 +5,19 @@ import facebook_scraper as scraper
 
 from profileData import cur_credentials, target_profiles
 
-target_profile = {'id': "123456789", 'name': "shirly.dwek"}
+target_profile = [{'id': "123456789", 'name': "shirly.dwek"}]
 date_format_string = "%Y-%m-%dT%H:%M:%S"
 
-target_posts = scraper.get_posts(
-    target_profile['name'], credentials=cur_credentials, pages=1)
+target_posts = [scraper.get_posts(curProfile['name'],
+                                  credentials=cur_credentials,
+                                  pages=1)
+                for curProfile
+                in target_profile]
 formatted_posts = [{'text': curPost['post_text'],
                     'publish_date': (curPost['time'].strftime(date_format_string)
                                      if curPost['time'] is not None
                                      else None),
                     'scraping_date': datetime.now().strftime(date_format_string)}
-                   for curPost in target_posts]
+                   for curPost
+                   in target_posts]
 print("done")
