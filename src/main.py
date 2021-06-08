f6a5b81923e9a39b8c36229803a5408a629a5b2c
daniel rogel <sagyrogel1@gmail.com>
2021-06-08 00:00:29 -0700

    feat: adjusts post fetching to handle multiple profiles.

--- src/main.py
+++ src/main.py
@@ -5,19 +5,24 @@ import facebook_scraper as scraper
 
 from profileData import cur_credentials, target_profiles
 
-target_profile = [{'id': "123456789", 'name': "shirly.dwek"}]
+target_profile = [{'id': "123456789", 'name': "shirly.dwek"}, {
+    'id': "123456799", 'name': "shirly.dwek"}]
 date_format_string = "%Y-%m-%dT%H:%M:%S"
 
-target_posts = [scraper.get_posts(curProfile['name'],
-                                  credentials=cur_credentials,
-                                  pages=1)
-                for curProfile
-                in target_profile]
-formatted_posts = [{'text': curPost['post_text'],
-                    'publish_date': (curPost['time'].strftime(date_format_string)
-                                     if curPost['time'] is not None
-                                     else None),
-                    'scraping_date': datetime.now().strftime(date_format_string)}
-                   for curPost
-                   in target_posts]
+target_post_groups = [[cur_post for cur_post in scraper.get_posts(cur_profile['name'],
+                                                                  credentials=cur_credentials,
+                                                                  pages=1)]
+                      for cur_profile
+                      in target_profile]
+
+formatted_post_groups = [cur_post_group[0]['post_text']
+                         for cur_post_group in target_post_groups]
+
+# formatted_posts = [{'text': curPost['post_text'],
+#                     'publish_date': (curPost['time'].strftime(date_format_string)
+#                                      if curPost['time'] is not None
+#                                      else None),
+#                     'scraping_date': datetime.now().strftime(date_format_string)}
+#                    for curPost
+#                    in target_posts]
 print("done")
