8350853148e1f23062c2588a0242214a63322e38
daniel rogel <sagyrogel1@gmail.com>
2021-06-08 02:31:47 -0700

    feat: adds post data to each post group.

--- src/main.py
+++ src/main.py
@@ -17,16 +17,17 @@ target_post_groups = [[dict(cur_post, **{'person_id': cur_profile['person_id']})
 
 formatted_post_groups = [{'person_id': cur_post_group[0]['person_id'],
                           'image': get_image_from_post_group(cur_post_group),
-                          'name': cur_post_group[0]['username']}
+                          'name': cur_post_group[0]['username'],
+                          'data': [{'text': curPost['post_text'],
+                                    'publish_date': (curPost['time'].strftime(date_format_string)
+                                                     if curPost['time'] is not None
+                                                     else None),
+                                    'scraping_date': datetime.now().strftime(date_format_string)}
+                                   for curPost
+                                   in cur_post_group]
+                          }
                          for cur_post_group
                          in target_post_groups
                          if len(cur_post_group) > 0]
 
-# formatted_posts = [{'text': curPost['post_text'],
-#                     'publish_date': (curPost['time'].strftime(date_format_string)
-#                                      if curPost['time'] is not None
-#                                      else None),
-#                     'scraping_date': datetime.now().strftime(date_format_string)}
-#                    for curPost
-#                    in target_posts]
 print("done")
